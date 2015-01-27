29cc51f18c4c9cda9a86a006fc9a1abab5fa2f2b
Steffen Gebert <steffen.gebert@typo3.org>
2015-01-27 19:59:33 +0100

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'steffen.gebert@typo3.org'
 license          'Apache2'
 description      'Installs/Configures site-notestypo3org'
 long_description 'Installs/Configures site-notestypo3org'
-version          '0.1.6'
+version          '0.1.7'
 
 depends "etherpad-lite", "= 0.2.5"
 depends "ssl_certificates"
