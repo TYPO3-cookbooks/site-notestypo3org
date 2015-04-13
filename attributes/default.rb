e00d409d8437244751a79e6fcc35f473b2cde7b9
Steffen Gebert <steffen.gebert@typo3.org>
2015-04-13 18:18:45 +0200

    Bump EP to 1.5.5

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default[:etherpadlite][:git][:reference] = "1.5.4"
+default[:etherpadlite][:git][:reference] = "1.5.5"
 
 default[:etherpadlite][:proxy][:hostname] = "notes.typo3.org"
 default[:etherpadlite][:proxy][:alias_hostnames] = [
