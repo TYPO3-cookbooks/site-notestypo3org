9b76eb8c4d26f4647312ec543916f385fe137a28
Steffen Gebert <steffen.gebert@typo3.org>
2015-11-19 18:33:02 +0100

    Pin etherpad-lite version to ~> 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures site-notestypo3org'
 long_description 'Installs/Configures site-notestypo3org'
 version          '0.1.13'
 
-depends "etherpad-lite", "= 0.2.5"
+depends "etherpad-lite", "~> 0.2.0"
 depends "ssl_certificates"
 depends "logrotate", "= 1.8.0"
