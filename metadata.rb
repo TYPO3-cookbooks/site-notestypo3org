ad13e442ccfa4d4e498a5e0ed417ca4c26d99351
Steffen Gebert <steffen.gebert@typo3.org>
2016-02-07 00:29:59 +0100

    Include recipes t3-base and t3-mysql::backup

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description      'Installs/Configures site-notestypo3org'
 long_description 'Installs/Configures site-notestypo3org'
 version          '0.2.1'
 
+depends "t3-base",         "~> 0.2.0"
+
 depends "t3-mysql",        "~> 0.1.0"
 
 depends "etherpad-lite",   "~> 0.2.0"
