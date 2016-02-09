3edc480a7eb44234be9e6e22a50b0c5d124f83bc
Steffen Gebert <steffen.gebert@typo3.org>
2016-02-09 19:56:37 +0100

    Do not pin logrotate, already pinned in t3-base

--- metadata.rb
+++ metadata.rb
@@ -9,8 +9,7 @@ version          '0.2.2'
 depends "t3-base",         "~> 0.2.0"
 
 depends "t3-mysql",        "~> 0.1.0"
-
 depends "etherpad-lite",   "~> 0.2.0"
+depends "logrotate"        # pinned in t3-base
 depends "ssl_certificates"
-depends "logrotate",       "= 1.8.0"
 
