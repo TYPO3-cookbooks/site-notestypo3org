80112a87f2a10606cbfea501c2f0f372bb23d175
Steffen Gebert <steffen.gebert@typo3.org>
2016-12-30 17:09:41 +0100

    [TASK] Update dependencies

--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,8 @@ version          '0.2.4'
 depends "t3-base",         "~> 0.2.0"
 
 depends "t3-mysql",        "~> 0.1.0"
-depends "etherpad-lite",   "~> 0.2.0"
+depends "build-essential", "= 4.0.0"
+depends "etherpad-lite",   "~> 1.0.0"
 depends "logrotate"        # pinned in t3-base
 depends "ssl_certificates"
 
