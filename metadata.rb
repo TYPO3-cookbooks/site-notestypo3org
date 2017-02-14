0502a147a150957575c3d08f391d875bf35475fc
Steffen Gebert <steffen.gebert@typo3.org>
2017-02-14 15:45:28 +0100

    [TASK] Update to etherpad-lite cookbook 2.0

--- metadata.rb
+++ metadata.rb
@@ -8,9 +8,9 @@ version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0
 
 depends "t3-base",         "~> 0.2.0"
 
+depends "etherpad-lite",   "~> 2.0.0"
 depends "t3-mysql",        "~> 0.1.0"
-depends "build-essential", "= 4.0.0"
-depends "etherpad-lite",   "~> 1.0.0"
-depends "logrotate"        # pinned in t3-base
 
-depends "chef_nginx", "= 5.0.2"
+depends "build-essential", "= 4.0.0"
+depends "chef_nginx",      "= 5.0.2"
+depends "logrotate"        # pinned in t3-base
